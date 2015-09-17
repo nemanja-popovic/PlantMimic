d829fdd05c8eeaedc3ece4f7d8cdbb9dfea20ece
Nemanja Popovic <nemanja23031991@yahoo.com>
2015-09-17 03:14:11 +0200

    Updated design of first page.

---
+++ client/app/first-page/first-page-pre-loader.directive.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+angular.module('plantMimicApp')
+    .directive('preLoader', function () {
+    return {
+        restrict: 'A',
+        link: function ($window) {
+            
+            // fade loading animation
+            angular.element("#object").delay(600).fadeOut(300);
+            // fade loading background					
+            angular.element("#loading").delay(1000).fadeOut(500);
+        }
+    }
+});
